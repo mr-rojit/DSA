86feb7f5610c4b5425e5a54d563f93c935d9fcd7
mr-rojit <rojit.1000@gmail.com>
2024-06-28 21:26:21 -0400

    max water container

--- 02_two_pointers_patterns/medium/max_water_container.py
+++ 02_two_pointers_patterns/medium/max_water_container.py
@@ -11,9 +11,39 @@ Output: 49
 Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 
 """
+# brute force
+def find_max_value_bf(arr):
+
+    n = len(arr)
+    width, max_ltr = 0, 0
+
+    for i in range(n):
+        for j in range(i+1, n):
+            width = j - i
+            height = min(arr[i], arr[j])
+            max_ltr = max(max_ltr, width*height)
+
+    return max_ltr
 
 def find_max_value(arr):
-    pass
+    n = len(arr)
+    max_area = 0
+    p1, p2 = 0, n-1
+    
+    while p1 < p2:
+        width = p2 - p1
+        height = min(arr[p1], arr[p2])
+        area = height * width
+        max_area = max(max_area, area)
+        
+        if arr[p1] <= arr[p2]:
+            p1+=1
+        else:
+            p2 -= 1
+
+    return max_area
+    
+
 
 arr = [1,8,6,2,5,4,8,3,7]
 print(find_max_value(arr))
