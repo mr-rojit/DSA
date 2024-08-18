70fc144b3803410f17284a2a36312acf6016d048
mr-rojit <rojit.1000@gmail.com>
2024-08-18 10:03:19 -0400

    bs

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,5 +1,5 @@
 
-def binary_search(arr, item):
+def binary_search_iterative(arr, item):
     n = len(arr)
     low, high = 0, n-1
 
@@ -15,8 +15,28 @@ def binary_search(arr, item):
     return -1
 
 
+
+def binary_search_recursive(arr, item, low, high):
+
+    mid = (high + low) //2
+
+    if low > high:
+        return -1
+    
+    if arr[mid] == item:
+        return mid
+    elif arr[mid] > item:
+        return binary_search_recursive(arr, item, low, mid-1)
+    else:
+        return binary_search_recursive(arr, item, mid+1, high)
+    
+
+
+
 arr = [1,3,5,7,14,16,19]
 
-idx = binary_search(arr, 31)
+# idx = binary_search_iterative(arr, 3)
+
+idx = binary_search_recursive(arr, 19, 0, len(arr)-1)
 
 print(idx)
