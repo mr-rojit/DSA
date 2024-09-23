fa7043f4ba7013b688702b6b3b3386b3d6f813b9
mr-rojit <rojit.1000@gmail.com>
2024-09-22 22:43:04 -0400

    commence grind 75

---
+++ 03_sliding_window_patterns/test/test.py
@@ -0,0 +1,6 @@
+s = "appleandballs"
+
+start_idx = 0
+
+for i in range(len(s) -2):
+    print(s[i:i+3])
---
+++ 2D_matrix/easy/rotate_matrix_element_wise.py
@@ -0,0 +1,18 @@
+"""
+Given a square matrix, the task is to rotate its elements clockwise by one step.
+
+Examples:
+
+    Input
+        1 2 3
+        4 5 6
+        7 8 9
+    Output:
+        4 1 2
+        7 5 3
+        8 9 6
+    
+    Input: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    
+    Output: 5 1 2 3 9 10 6 4 13 11 7 8 14 15 16 12
+"""
